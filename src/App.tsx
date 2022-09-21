ce63928f6e6d174db268e889cbc44fdcf3a657ef
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-09-21 14:14:45 -0300

    Fix GlobalStyle import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Header } from "./components/Header";
 import { Router } from "./Router";
 
 import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/themes/global";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
